4d6f8d07b6131615a24ab5f803d48ff9428623c7
jasonPonda <jasonvinda@gmail.com>
2022-10-26 14:41:08 +0200

    modification

--- src/components/search.js
+++ src/components/search.js
@@ -1,6 +1,12 @@
 import { useNavigate } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Collapse from "react-bootstrap/Collapse";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import { useState } from "react";
 
 const SearchBar = ({ searchQuery, setSearchQuery }) => {
+  const [redVal, setRedVal] = useState(true);
   const history = useNavigate();
   const onSubmit = (e) => {
     history.push(`?=${searchQuery}`);
@@ -12,13 +18,31 @@ const SearchBar = ({ searchQuery, setSearchQuery }) => {
         <span className="visually-hidden">Search blog posts</span>
       </label>
       <input
+        className="search"
         value={searchQuery}
         onInput={(e) => setSearchQuery(e.target.value)}
         type="text"
         placeholder="Search blog posts"
         name="s"
       />
-      <button type="submit">Search</button>
+      <input className="btn-search" type="submit" value="Search" />
+
+      <div>
+        <Button variant="secondary" onClick={() => setRedVal(!redVal)}>
+          Filter
+        </Button>
+        <Collapse in={redVal} dimension="width">
+          <div>
+            <div
+              style={{
+                width: "200px",
+                height: "200px",
+                backgroundColor: "red",
+              }}
+            ></div>
+          </div>
+        </Collapse>
+      </div>
     </form>
   );
 };
